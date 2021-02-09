4bd95fc9f198086696b3148986654745329b0e4d
santosjioiain <78665588+santosjioiain@users.noreply.github.com>
2021-02-09 11:08:50 +0800

    Add files via upload

---
+++ Models.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebScraping
+{
+    public class Models
+    {
+        public string User { get; set; }
+        public string EventDescription { get; set; }
+        public string EventTime { get; set; }
+    }
+}
