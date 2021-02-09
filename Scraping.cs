f75f5e3edb9d529018abfbf8e291bedab9385a64
santosjioiain <78665588+santosjioiain@users.noreply.github.com>
2021-02-09 11:10:05 +0800

    Add files via upload

---
+++ Scraping.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.IO;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebScraping
+{
+    public class Scraping
+    {
+        HttpClient client = new HttpClient();
+
+        private static async Task<string> CallUrl(string fullUrl)
+        {
+            HttpClient client = new HttpClient();
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls13;
+            client.DefaultRequestHeaders.Accept.Clear();
+            var response = client.GetStringAsync(fullUrl);
+            return await response;
+        }
+        public IActionResult Index()
+        {
+            string url = "https://en.wikipedia.org/wiki/List_of_programmers";
+            var response = CallUrl(url).Result;
+            return View();
+        }
+    }
+}
